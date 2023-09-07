b90c4b2b0818fb554f98b59791717e090000a48a
nonnodacciaio <nonnodacciaio@gmail.com>
2023-09-07 20:26:48 +0200

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -12,9 +17,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'task-manager-addd4',
+    appId: '1:486836021503:web:bbf7d7ed27d801741a04ca',
+    databaseURL: 'https://task-manager-addd4-default-rtdb.firebaseio.com',
+    storageBucket: 'task-manager-addd4.appspot.com',
+    apiKey: 'AIzaSyDrpJO5OiFrtj-vLdwXXJ1H5BUTR5drNpc',
+    authDomain: 'task-manager-addd4.firebaseapp.com',
+    messagingSenderId: '486836021503',
+    measurementId: 'G-D2YNF6CG2Z',
+  },
+};
