36a877dc46831d3ebabf2b782637eea6f5c5bcd3
nonnodacciaio <nonnodacciaio@gmail.com>
2023-09-09 15:27:53 +0200

    Begun adding tasks

--- src/app/modules/home/components/task-list.component.ts
+++ src/app/modules/home/components/task-list.component.ts
@@ -1,4 +1,50 @@
 import { Component } from "@angular/core";
+import { Firestore } from "@angular/fire/firestore";
+import { AuthService } from "src/app/shared/services/auth.service";
 
-@Component({ selector: "task-list", template: `Task list works` })
-export class TasklistComponent {}
+@Component({
+	selector: "task-list",
+	template: `<mat-list *ngIf="authService.isLoggedIn"
+			><mat-list-item *ngFor="let task of tasks">{{ task.value }}</mat-list-item></mat-list
+		>
+		<mat-form-field
+			><input
+				matInput
+				[(ngModel)]="newTask"
+				type="text" /></mat-form-field
+		><button
+			mat-button
+			color="primary"
+			[disabled]="newTask == ''"
+			(click)="addTask()">
+			Add task
+		</button>`
+})
+export class TasklistComponent {
+	tasks: { value: string; complete: boolean }[] = [];
+	newTask: string = "";
+
+	constructor(public authService: AuthService, private firestore: Firestore) {}
+
+	addTask() {
+		const uid = this.authService.userData?.uid; // Replace with your authentication logic
+		if (uid) {
+			const taskObject = { value: this.newTask, completed: false };
+
+			// Add the task to Firestore under the "tasks" array for the user
+			this.firestore
+				.collection("users")
+				.doc(uid)
+				.update({
+					tasks: firebase.firestore.FieldValue.arrayUnion(taskObject)
+				})
+				.then(() => {
+					// Clear the input field after adding the task
+					this.newTask = "";
+				})
+				.catch(error => {
+					console.error("Error adding task:", error);
+				});
+		}
+	}
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,24 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { HomePage } from "./home.page";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { TasklistComponent } from "./components/task-list.component";
+import { HomePage } from "./home.page";
+import { Routes, RouterModule } from "@angular/router";
+import { MatListModule } from "@angular/material/list";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+
+const routes: Routes = [
+	{
+		path: "",
+		component: HomePage
+	}
+];
 
 @NgModule({
 	declarations: [HomePage, TasklistComponent],
-	imports: [CommonModule],
-	providers: []
+	imports: [CommonModule, RouterModule.forChild(routes), MatListModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+	providers: [AngularFireAuth]
 })
 export class HomeModule {}
