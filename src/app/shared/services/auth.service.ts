d7e528a8435d58befb5e80b4d1a54490ec68262f
nonnodacciaio <nonnodacciaio@gmail.com>
2023-09-09 13:46:14 +0200

    Added firestore

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -4,12 +4,13 @@ import { Router } from "@angular/router";
 import { FirebaseError } from "firebase/app";
 import { User } from "src/app/models/user.model";
 import { MessageService } from "./message.service";
+import { Firestore, doc, setDoc } from "@angular/fire/firestore";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
 	userData: User | null = null;
 
-	constructor(private afAuth: AngularFireAuth, private messageService: MessageService, private router: Router) {
+	constructor(private afAuth: AngularFireAuth, private messageService: MessageService, private router: Router, private firestore: Firestore) {
 		this.afAuth.authState.subscribe(user => {
 			if (user) {
 				this.userData = user as User;
@@ -32,7 +33,10 @@ export class AuthService {
 	}
 
 	login(email: string, password: string) {
-		this.afAuth.signInWithEmailAndPassword(email, password).catch((error: FirebaseError) => this.messageService.error(error.message));
+		this.afAuth
+			.signInWithEmailAndPassword(email, password)
+			.then(result => this.setUserData(result.user))
+			.catch((error: FirebaseError) => this.messageService.error(error.message));
 	}
 
 	signup(email: string, password: string) {
@@ -40,8 +44,14 @@ export class AuthService {
 			.createUserWithEmailAndPassword(email, password)
 			.then(result => {
 				this.sendVerificationMail();
+				this.setUserData(result.user);
 			})
 			.catch((error: FirebaseError) => this.messageService.error(error.message));
+	}
+
+	private async setUserData(user: any) {
+		const docRef = doc(this.firestore, "users", user.uid);
+		await setDoc(docRef, { email: user.email, uid: user.uid, emailVerified: user.emailVerified, isAnonymous: user.isAnonymous }, { merge: true });
 	}
 
 	logout() {
